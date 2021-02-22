38a2e273c40931d520675dae10bc9242507e2185
Aimeos <aimeos@aimeos.org>
2021-02-22 10:39:15 +0100

    Removed unused parameters

--- admin/jqadm/tests/Admin/JQAdm/Cms/StandardTest.php
+++ admin/jqadm/tests/Admin/JQAdm/Cms/StandardTest.php
@@ -128,7 +128,7 @@ class StandardTest extends \PHPUnit\Framework\TestCase
 
 	public function testGetViewException()
 	{
-		$object = new \Aimeos\Admin\JQAdm\Cms\Standard( $this->context, [] );
+		$object = new \Aimeos\Admin\JQAdm\Cms\Standard( $this->context );
 
 		$this->expectException( \Aimeos\Admin\JQAdm\Exception::class );
 		$object->getView();

1315361f79c85f5038bde9458eaaf055b3a2d8b6
Aimeos <aimeos@aimeos.org>
2022-03-01 08:07:32 +0100

    Allow column components in background elements

--- admin/jqadm/themes/custom.js
+++ admin/jqadm/themes/custom.js
@@ -463,7 +463,7 @@ Aimeos.CMSContent = {
 					model: {
 						defaults: {
 							tagName: 'div',
-							draggable: '.container-xl, .container-fluid, .col',
+							draggable: '.container-xl, .container-fluid, .col, .background',
 							droppable: true,
 							attributes: {
 								class: 'row',

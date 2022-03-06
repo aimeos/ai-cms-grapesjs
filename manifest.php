9c5f64bfdf74a8f5cd8cd895ac0be739159f8f2f
Aimeos <aimeos@aimeos.org>
2022-03-06 11:25:37 +0100

    Simplified extension structure

--- manifest.php
+++ manifest.php
@@ -11,44 +11,31 @@ return [
 		'ai-controller-frontend',
 	],
 	'include' => [
-		'lib/custom/src',
-		'client/html/src',
-		'client/jsonapi/src',
-		'controller/frontend/src',
-		'admin/jsonadm/src',
-		'admin/jqadm/src',
+		'src',
 	],
 	'i18n' => [
 		'admin' => 'admin/i18n',
-		'admin/jsonadm' => 'admin/jsonadm/i18n',
-		'client' => 'client/i18n',
-		'client/code' => 'client/i18n/code',
-		'controller/frontend' => 'controller/frontend/i18n',
-		'mshop' => 'lib/custom/i18n',
 	],
 	'config' => [
 		'config',
 	],
 	'setup' => [
-		'lib/custom/setup',
+		'setup',
 	],
 	'template' => [
 		'admin/jqadm/templates' => [
-			'admin/jqadm/templates',
-		],
-		'admin/jsonadm/templates' => [
-			'admin/jsonadm/templates',
+			'templates/admin/jqadm',
 		],
 		'client/html/templates' => [
-			'client/html/templates',
+			'templates/client/html',
 		],
 		'client/jsonapi/templates' => [
-			'client/jsonapi/templates',
+			'templates/client/jsonapi',
 		],
 	],
 	'custom' => [
 		'admin/jqadm' => [
-			'admin/jqadm/manifest.jsb2',
+			'manifest.jsb2',
 		],
 	],
 ];

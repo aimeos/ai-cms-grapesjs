ee18f16c24f31cc65316e0dfdba47a42324ccc6d
Aimeos <aimeos@aimeos.org>
2022-08-17 15:58:10 +0200

    Fixed delete and batch operations in list view

--- templates/admin/jqadm/cms/list.php
+++ templates/admin/jqadm/cms/list.php
@@ -87,7 +87,9 @@ $columnList = [
 
 <div class="list-view"
 	data-domain="cms"
-	data-items="<?= $enc->attr( $this->get( 'items', map() )->call( 'toArray' )->all() ) ?>">
+	data-siteid="<?= $enc->attr( $this->site()->siteid() ) ?>"
+	data-filter="<?= $enc->attr( $this->session( 'aimeos/admin/jqadm/cms/filter', new \stdClass ) ) ?>"
+	data-items="<?= $enc->attr( $this->get( 'items', map() )->call( 'toArray', [true] )->all() ) ?>">
 
 	<nav class="main-navbar">
 
@@ -118,8 +120,7 @@ $columnList = [
 	?>
 
 	<form ref="form" class="list list-cms" method="POST"
-		action="<?= $enc->attr( $this->url( $target, $controller, $action, $searchParams, [], $config ) ) ?>"
-		data-deleteurl="<?= $enc->attr( $this->url( $delTarget, $delCntl, $delAction, $params, [], $delConfig ) ) ?>">
+		action="<?= $enc->attr( $this->url( $target, $controller, $action, $searchParams, [], $config ) ) ?>">
 
 		<?= $this->csrf()->formfield() ?>
 
@@ -131,106 +132,183 @@ $columnList = [
 			v-on:close="columns = false">
 		</column-select>
 
-		<table class="list-items table table-hover table-striped">
-			<thead class="list-header">
-				<tr>
-					<th class="select">
-						<a href="#" class="btn act-delete fa" tabindex="1"
-							v-on:click.prevent.stop="askDelete()"
-							title="<?= $enc->attr( $this->translate( 'admin', 'Delete selected entries' ) ) ?>"
-							aria-label="<?= $enc->attr( $this->translate( 'admin', 'Delete' ) ) ?>">
-						</a>
-					</th>
+		<div class="table-responsive">
+			<table class="list-items table table-hover table-striped">
+				<thead class="list-header">
+					<tr>
+						<th class="select">
+							<button class="btn icon-menu" type="button" data-bs-toggle="dropdown"
+								aria-expanded="false" title="<?= $enc->attr( $this->translate( 'admin', 'Menu' ) ) ?>">
+							</button>
+							<ul class="dropdown-menu">
+								<li>
+									<a class="btn" v-on:click.prevent="batch = true" href="#" tabindex="1">
+										<?= $enc->html( $this->translate( 'admin', 'Edit' ) ) ?>
+									</a>
+								</li>
+								<li>
+									<a class="btn" v-on:click.prevent="askDelete(null, $event)" tabindex="1"
+										href="<?= $enc->attr( $this->link( 'admin/jqadm/url/delete', $params ) ) ?>">
+										<?= $enc->html( $this->translate( 'admin', 'Delete' ) ) ?>
+									</a>
+								</li>
+							</ul>
+						</th>
+
+						<?= $this->partial(
+								$this->config( 'admin/jqadm/partial/listhead', 'listhead' ),
+								['fields' => $fields, 'params' => $params, 'data' => $columnList, 'sort' => $this->session( 'aimeos/admin/jqadm/cms/sort' )]
+							);
+						?>
+
+						<th class="actions">
+							<a class="btn fa act-add" tabindex="1"
+								href="<?= $enc->attr( $this->link( 'admin/jqadm/url/create', $params ) ) ?>"
+								title="<?= $enc->attr( $this->translate( 'admin', 'Insert new entry (Ctrl+I)' ) ) ?>"
+								aria-label="<?= $enc->attr( $this->translate( 'admin', 'Add' ) ) ?>">
+							</a>
+
+							<a class="btn act-columns fa" href="#" tabindex="<?= $this->get( 'tabindex', 1 ) ?>"
+								title="<?= $enc->attr( $this->translate( 'admin', 'Columns' ) ) ?>"
+								v-on:click.prevent.stop="columns = true">
+							</a>
+						</th>
+					</tr>
+				</thead>
+				<tbody>
 
 					<?= $this->partial(
-							$this->config( 'admin/jqadm/partial/listhead', 'listhead' ),
-							['fields' => $fields, 'params' => $params, 'data' => $columnList, 'sort' => $this->session( 'aimeos/admin/jqadm/cms/sort' )]
-						);
+						$this->config( 'admin/jqadm/partial/listsearch', 'listsearch' ), [
+							'fields' => array_merge( $fields, ['select'] ), 'filter' => $this->session( 'aimeos/admin/jqadm/cms/filter', [] ),
+							'data' => [
+								'cms.id' => ['op' => '=='],
+								'cms.status' => ['op' => '==', 'type' => 'select', 'val' => [
+									'1' => $this->translate( 'mshop/code', 'status:1' ),
+									'0' => $this->translate( 'mshop/code', 'status:0' ),
+									'-1' => $this->translate( 'mshop/code', 'status:-1' ),
+									'-2' => $this->translate( 'mshop/code', 'status:-2' ),
+								]],
+								'cms.url' => [],
+								'cms.label' => [],
+								'cms.ctime' => ['op' => '-', 'type' => 'datetime-local'],
+								'cms.mtime' => ['op' => '-', 'type' => 'datetime-local'],
+								'cms.editor' => [],
+							]
+						] );
 					?>
 
-					<th class="actions">
-						<a class="btn fa act-add" tabindex="1"
-							href="<?= $enc->attr( $this->url( $newTarget, $newCntl, $newAction, $params, [], $newConfig ) ) ?>"
-							title="<?= $enc->attr( $this->translate( 'admin', 'Insert new entry (Ctrl+I)' ) ) ?>"
-							aria-label="<?= $enc->attr( $this->translate( 'admin', 'Add' ) ) ?>">
-						</a>
-
-						<a class="btn act-columns fa" href="#" tabindex="<?= $this->get( 'tabindex', 1 ) ?>"
-							title="<?= $enc->attr( $this->translate( 'admin', 'Columns' ) ) ?>"
-							v-on:click.prevent.stop="columns = true">
-						</a>
-					</th>
-				</tr>
-			</thead>
-			<tbody>
-
-				<?= $this->partial(
-					$this->config( 'admin/jqadm/partial/listsearch', 'listsearch' ), [
-						'fields' => array_merge( $fields, ['select'] ), 'filter' => $this->session( 'aimeos/admin/jqadm/cms/filter', [] ),
-						'data' => [
-							'cms.id' => ['op' => '=='],
-							'cms.status' => ['op' => '==', 'type' => 'select', 'val' => [
-								'1' => $this->translate( 'mshop/code', 'status:1' ),
-								'0' => $this->translate( 'mshop/code', 'status:0' ),
-								'-1' => $this->translate( 'mshop/code', 'status:-1' ),
-								'-2' => $this->translate( 'mshop/code', 'status:-2' ),
-							]],
-							'cms.url' => [],
-							'cms.label' => [],
-							'cms.ctime' => ['op' => '-', 'type' => 'datetime-local'],
-							'cms.mtime' => ['op' => '-', 'type' => 'datetime-local'],
-							'cms.editor' => [],
-						]
-					] );
-				?>
-
-				<?php foreach( $this->get( 'items', [] ) as $id => $item ) : ?>
-					<?php $url = $enc->attr( $this->url( $getTarget, $getCntl, $getAction, ['id' => $id] + $params, [], $getConfig ) ) ?>
-					<tr class="list-item <?= $this->site()->readonly( $item->getSiteId() ) ?>" data-label="<?= $enc->attr( $item->getLabel() ) ?>">
-						<td class="select"><input v-on:click="toggle(`<?= $enc->js( $id ) ?>`)" v-bind:checked="items[`<?= $enc->js( $id ) ?>`].checked" class="form-check-input" type="checkbox" tabindex="1" name="<?= $enc->attr( $this->formparam( ['id', ''] ) ) ?>" value="<?= $enc->attr( $item->getId() ) ?>" /></td>
-						<?php if( in_array( 'cms.id', $fields ) ) : ?>
-							<td class="cms-id"><a class="items-field" href="<?= $url ?>"><?= $enc->html( $item->getId() ) ?></a></td>
-						<?php endif ?>
-						<?php if( in_array( 'cms.status', $fields ) ) : ?>
-							<td class="cms-status"><a class="items-field" href="<?= $url ?>"><div class="fa status-<?= $enc->attr( $item->getStatus() ) ?>"></div></a></td>
-						<?php endif ?>
-						<?php if( in_array( 'cms.url', $fields ) ) : ?>
-							<td class="cms-url"><a class="items-field" href="<?= $url ?>"><?= $enc->html( $item->getUrl() ) ?></a></td>
-						<?php endif ?>
-						<?php if( in_array( 'cms.label', $fields ) ) : ?>
-							<td class="cms-label"><a class="items-field" href="<?= $url ?>"><?= $enc->html( $item->getLabel() ) ?></a></td>
-						<?php endif ?>
-						<?php if( in_array( 'cms.ctime', $fields ) ) : ?>
-							<td class="cms-ctime"><a class="items-field" href="<?= $url ?>"><?= $enc->html( $item->getTimeCreated() ) ?></a></td>
-						<?php endif ?>
-						<?php if( in_array( 'cms.mtime', $fields ) ) : ?>
-							<td class="cms-mtime"><a class="items-field" href="<?= $url ?>"><?= $enc->html( $item->getTimeModified() ) ?></a></td>
-						<?php endif ?>
-						<?php if( in_array( 'cms.editor', $fields ) ) : ?>
-							<td class="cms-editor"><a class="items-field" href="<?= $url ?>"><?= $enc->html( $item->editor() ) ?></a></td>
-						<?php endif ?>
-
-						<td class="actions">
-							<a class="btn act-copy fa" tabindex="1"
-								href="<?= $enc->attr( $this->url( $copyTarget, $copyCntl, $copyAction, ['id' => $id] + $params, [], $copyConfig ) ) ?>"
-								title="<?= $enc->attr( $this->translate( 'admin', 'Copy this entry' ) ) ?>"
-								aria-label="<?= $enc->attr( $this->translate( 'admin', 'Copy' ) ) ?>">
-							</a>
-							<?php if( !$this->site()->readonly( $item->getSiteId() ) ) : ?>
-								<a class="btn act-delete fa" tabindex="1" href="#"
-									v-on:click.prevent.stop="askDelete(`<?= $enc->js( $id ) ?>`)"
-									title="<?= $enc->attr( $this->translate( 'admin', 'Delete this entry' ) ) ?>"
-									aria-label="<?= $enc->attr( $this->translate( 'admin', 'Delete' ) ) ?>">
+					<tr class="batch" style="display: none" v-show="batch">
+						<td colspan="<?= count( $fields ) + 2 ?>">
+							<div class="batch-header">
+								<div class="intro">
+									<span class="name"><?= $enc->html( $this->translate( 'admin', 'Bulk edit' ) ) ?></span>
+									<span class="count">{{ selected }} <?= $enc->html( $this->translate( 'admin', 'selected' ) ) ?></span>
+								</div>
+								<a class="btn btn-secondary" href="#" v-on:click.prevent="batch = false">
+									<?= $enc->html( $this->translate( 'admin', 'Close' ) ) ?>
 								</a>
-							<?php endif ?>
+							</div>
+							<div class="card">
+								<div class="card-header">
+									<span><?= $enc->html( $this->translate( 'admin', 'Basic' ) ) ?></span>
+									<button class="btn btn-primary" formaction="<?= $enc->attr( $this->link( 'admin/jqadm/url/batch', ['resource' => 'cms'] ) ) ?>">
+										<?= $enc->html( $this->translate( 'admin', 'Save' ) ) ?>
+									</button>
+								</div>
+								<div class="card-body">
+									<div class="row">
+										<div class="col-lg-6">
+											<div class="row">
+												<div class="col-1">
+													<input id="batch-cms-status" class="form-check-input" type="checkbox" v-on:click="setState('item/cms.status')" />
+												</div>
+												<label class="col-4 form-control-label" for="batch-cms-status">
+													<?= $enc->html( $this->translate( 'admin', 'Status' ) ) ?>
+												</label>
+												<div class="col-7">
+													<select class="form-select" v-bind:disabled="state('item/cms.status')"
+														name="<?= $enc->attr( $this->formparam( array( 'item', 'cms.status' ) ) ) ?>">
+														<option value=""></option>
+														<option value="1"><?= $enc->html( $this->translate( 'mshop/code', 'status:1' ) ) ?></option>
+														<option value="0"><?= $enc->html( $this->translate( 'mshop/code', 'status:0' ) ) ?></option>
+														<option value="-1"><?= $enc->html( $this->translate( 'mshop/code', 'status:-1' ) ) ?></option>
+														<option value="-2"><?= $enc->html( $this->translate( 'mshop/code', 'status:-2' ) ) ?></option>
+													</select>
+												</div>
+											</div>
+										</div>
+										<div class="col-lg-6">
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="batch-footer">
+								<a class="btn btn-secondary" href="#" v-on:click.prevent="batch = false">
+									<?= $enc->html( $this->translate( 'admin', 'Close' ) ) ?>
+								</a>
+								<button class="btn btn-primary" formaction="<?= $enc->attr( $this->link( 'admin/jqadm/url/batch', ['resource' => 'cms'] ) ) ?>">
+									<?= $enc->html( $this->translate( 'admin', 'Save' ) ) ?>
+								</button>
+							</div>
 						</td>
 					</tr>
-				<?php endforeach ?>
-			</tbody>
-		</table>
+
+					<?php foreach( $this->get( 'items', [] ) as $id => $item ) : ?>
+						<?php $url = $enc->attr( $this->link( 'admin/jqadm/url/get', ['id' => $id] + $params ) ) ?>
+						<tr class="list-item <?= $this->site()->readonly( $item->getSiteId() ) ?>" data-label="<?= $enc->attr( $item->getLabel() ) ?>">
+							<td class="select">
+								<input class="form-check-input" type="checkbox" tabindex="1"
+									name="<?= $enc->attr( $this->formparam( ['id', ''] ) ) ?>"
+									value="<?= $enc->attr( $item->getId() ) ?>"
+									v-on:click="toggle(`<?= $enc->js( $id ) ?>`)"
+									v-bind:checked="checked(`<?= $enc->js( $id ) ?>`)"
+									v-bind:disabled="readonly(`<?= $enc->js( $id ) ?>`)" />
+							</td>
+							<?php if( in_array( 'cms.id', $fields ) ) : ?>
+								<td class="cms-id"><a class="items-field" href="<?= $url ?>"><?= $enc->html( $item->getId() ) ?></a></td>
+							<?php endif ?>
+							<?php if( in_array( 'cms.status', $fields ) ) : ?>
+								<td class="cms-status"><a class="items-field" href="<?= $url ?>"><div class="fa status-<?= $enc->attr( $item->getStatus() ) ?>"></div></a></td>
+							<?php endif ?>
+							<?php if( in_array( 'cms.url', $fields ) ) : ?>
+								<td class="cms-url"><a class="items-field" href="<?= $url ?>"><?= $enc->html( $item->getUrl() ) ?></a></td>
+							<?php endif ?>
+							<?php if( in_array( 'cms.label', $fields ) ) : ?>
+								<td class="cms-label"><a class="items-field" href="<?= $url ?>"><?= $enc->html( $item->getLabel() ) ?></a></td>
+							<?php endif ?>
+							<?php if( in_array( 'cms.ctime', $fields ) ) : ?>
+								<td class="cms-ctime"><a class="items-field" href="<?= $url ?>"><?= $enc->html( $item->getTimeCreated() ) ?></a></td>
+							<?php endif ?>
+							<?php if( in_array( 'cms.mtime', $fields ) ) : ?>
+								<td class="cms-mtime"><a class="items-field" href="<?= $url ?>"><?= $enc->html( $item->getTimeModified() ) ?></a></td>
+							<?php endif ?>
+							<?php if( in_array( 'cms.editor', $fields ) ) : ?>
+								<td class="cms-editor"><a class="items-field" href="<?= $url ?>"><?= $enc->html( $item->editor() ) ?></a></td>
+							<?php endif ?>
+
+							<td class="actions">
+								<a class="btn act-copy fa" tabindex="1"
+								href="<?= $enc->attr( $this->link( 'admin/jqadm/url/copy', ['id' => $id] + $params ) ) ?>"
+									title="<?= $enc->attr( $this->translate( 'admin', 'Copy this entry' ) ) ?>"
+									aria-label="<?= $enc->attr( $this->translate( 'admin', 'Copy' ) ) ?>">
+								</a>
+								<?php if( !$this->site()->readonly( $item->getSiteId() ) ) : ?>
+									<a class="btn act-delete fa" tabindex="1"
+										v-on:click.prevent.stop="askDelete(`<?= $enc->js( $id ) ?>`, $event)"
+										href="<?= $enc->attr( $this->link( 'admin/jqadm/url/delete', $params ) ) ?>"
+										title="<?= $enc->attr( $this->translate( 'admin', 'Delete this entry' ) ) ?>"
+										aria-label="<?= $enc->attr( $this->translate( 'admin', 'Delete' ) ) ?>">
+									</a>
+								<?php endif ?>
+							</td>
+						</tr>
+					<?php endforeach ?>
+				</tbody>
+			</table>
+		</div>
 
 		<?php if( $this->get( 'items', map() )->isEmpty() ) : ?>
-			<div class="noitems"><?= $enc->html( sprintf( $this->translate( 'admin', 'No items found' ) ) ) ?></div>
+			<?= $enc->html( sprintf( $this->translate( 'admin', 'No items found' ) ) ) ?>
 		<?php endif ?>
 	</form>
 

7f5003da2339627d171b4d575272c2642869f69e
Aimeos <aimeos@aimeos.org>
2022-11-18 03:09:49 +0100

    Adapt to change DB factory parameter

--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -58,7 +58,7 @@ class TestHelper
 		$ctx->setConfig( $conf );
 
 
-		$dbm = \Aimeos\Base\DB\Factory::create( $conf, 'DBAL' );
+		$dbm = \Aimeos\Base\DB\Factory::create( $conf->get( 'resource', [] ), 'DBAL' );
 		$ctx->setDatabaseManager( $dbm );
 
 

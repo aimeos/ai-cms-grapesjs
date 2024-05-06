3e281684672d78c3bc1bc4b1914bd0e605cff7cf
Aimeos <aimeos@aimeos.org>
2024-05-06 14:04:27 +0200

    Added type data for unit tests

---
+++ setup/unittest/data/type.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @license LGPLv3, https://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2021-2024
+ */
+
+return [
+	'cms/lists/type' => [
+		['domain' => 'text', 'code' => 'default', 'label' => 'Standard', 'status' => 1],
+		['domain' => 'media', 'code' => 'default', 'label' => 'Standard', 'status' => 1],
+	],
+
+	'text/type' => [
+		['domain' => 'cms', 'code' => 'name', 'label' => 'Name', 'status' => 1],
+		['domain' => 'cms', 'code' => 'meta-keyword', 'label' => 'Meta keywords', 'status' => 1],
+		['domain' => 'cms', 'code' => 'meta-description', 'label' => 'Meta description', 'status' => 1],
+		['domain' => 'cms', 'code' => 'content', 'label' => 'Content', 'status' => 1],
+	],
+
+	'media/type' => [
+		['domain' => 'cms', 'code' => 'default', 'label' => 'Standard', 'status' => 1],
+	]
+];
